2ce8ee92d6a563d754f7de795ea7fad7f7cefcf0
tommyfricano <tjfricano@gmail.com>
2024-08-13 14:15:54 -0400

    pipe

--- .github/scripts/upload_to_confluence.py
+++ .github/scripts/upload_to_confluence.py
@@ -12,23 +12,55 @@ CONFLUENCE_API_TOKEN = os.environ.get("CONFLUENCE_API_TOKEN")
 with open("README.md", "r") as file:
     readme_content = file.read()
 
-    payload = {
-        "version": {"number": 2},
-        "title": "{CONFLUENCE_PAGE_TITLE}",
-         "type": "page",
-         "body": {
-            "storage": {
-                "value": f"<pre>{readme_content}</pre>",
-                "representation": "storage",
-            }
-         },
-    }
+
+table_html = """
+<table>
+    <tr>
+        <th>Column 1</th>
+        <th>Column 2</th>
+    </tr>
+    <tr>
+        <td>Value 1</td>
+        <td>Value 2</td>
+    </tr>
+    <tr>
+        <td>Value 3</td>
+        <td>Value 4</td>
+    </tr>
+</table>
+"""
+
+# Create the payload with the table and the README content
+payload = {
+    "version": {"number": 2},
+    "title": "{CONFLUENCE_PAGE_TITLE}",
+    "type": "page",
+    "body": {
+        "storage": {
+            "value": f"<pre>{readme_content}</pre><br/>{table_html}",
+            "representation": "storage",
+        }
+    },
+}
+#     payload = {
+#         "version": {"number": 2},
+#         "title": "{CONFLUENCE_PAGE_TITLE}",
+#          "type": "page",
+#          "body": {
+#             "storage": {
+#                 "value": f"<pre>{readme_content}</pre>",
+#                 "representation": "storage",
+#             }
+#          },
+#     }
     response = requests.put(
         f"{CONFLUENCE_URL}/{CONFLUENCE_PAGE_ID}",
         json=payload,
         headers={"Content-Type": "application/json",
                  "Authorization": f"Basic {CONFLUENCE_API_TOKEN}"},
     )
+
+
 
 if response.status_code >= 300:
     print(f"Failed to upload README.md to Confluence: {response.status_code}")
